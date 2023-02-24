ce46f609b286dae2ff5ecd4c22f5dcd4db094c0d
sumaiyamai <sumaiya.muhammad@aun.edu.ng>
2023-02-24 14:18:45 -0800

    fixQ0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,13 @@
  * main - Prints number and states whether positive, negative, or zero.
  * Return: Always 0
 */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
