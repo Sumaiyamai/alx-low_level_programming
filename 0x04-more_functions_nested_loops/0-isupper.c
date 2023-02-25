4646025bc5b041b5845698c07bb1c7530be34ce7
sumaiyamai <sumaiya.muhammad@aun.edu.ng>
2023-02-25 03:06:37 -0800

    fixQ0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *is_upper - if  uppercase character return 1, if not return 0,
+ *
+ * @c this is  entry
+ *
+ * return 0,
+ */
+
+
+	int _isupper(int c)
+	
+	int = c
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
