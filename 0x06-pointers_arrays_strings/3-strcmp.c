367a1223bfe2167e142507431dd3cbe51eeafb09
sumaiyamai <sumaiya.muhammad@aun.edu.ng>
2023-03-02 11:47:57 -0800

    fixing files

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,12 +6,13 @@
  * @s1: Is the input string
  * @s2: Is the input string
  *
- * Return: return the strings if they  are equals  "0", if not return other number
+ * Return: return the strings if they  are equals "0",
+ * if not return other number
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++){;}
+	for ((*s1 != '\0' && *s2 != '\0') && (*s1 == *s2) s1++, s2++)
 
 	if (*s1 == *s2)
 	{
