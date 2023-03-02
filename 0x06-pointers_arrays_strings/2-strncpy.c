21c8942238f1e127717d1b458d569767fa03e9db
sumaiyamai <sumaiya.muhammad@aun.edu.ng>
2023-03-02 04:29:50 -0800

    fixing files

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ *
+ * @dest: is the output
+ * @src: is the input
+ * @n: number of bytes to copy
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; index < n && src[index] != '\0' ; index++)
+	{
+		dest[index] = src[index];
+	}
+	for (; index  < n; index++)
+	{
+		dest[index] = '\0';
+	}
+	return (dest);
+}
+
