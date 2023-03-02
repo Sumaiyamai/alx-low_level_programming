a08cb2696bfa838a5070d23eb072a87609b29403
sumaiyamai <sumaiya.muhammad@aun.edu.ng>
2023-03-02 13:40:20 -0800

    fixing files

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,29 +2,30 @@
 
 /**
  * _strncat - function that concatenates two strings
- *  that will use at most n bytes from src
+ *            that will use at most n bytes from src
  *
  *  @dest: is the output
  *  @src: is the input src
  *  @n: number of bytes from source
- *  Return: Always 0
+ *
+ *  return: return pointer to dest
  *
  */
 char *_strncat(char *dest, char *src, int n);
 {
 
-	int index, count;
+	int a, b;
 
-	for (index = 0; dest[index] != '\0'; index++)
+	while (a = 0; dest[a] != '\0'; a++)
 		;
-	for (count = 0; count < n && src[count] != '\0'; count++)
+	while (b = 0; b < n && src[b] != '\0'; b++)
 	{
-		dest[index] = src[count];
-		ndex++;
+		dest[a] = src[b];
+		a++;
 	}
-	if (count < n)
+	if (b < n)
 	{
-	dest[index] = '\0';
+	dest[a] = '\0';
 	}
 	return (dest);
 }
