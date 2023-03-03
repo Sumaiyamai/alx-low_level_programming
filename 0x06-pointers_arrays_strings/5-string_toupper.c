4f596803152acfe15f0ab90c1d39c3a2909293ee
sumaiyamai <sumaiya.muhammad@aun.edu.ng>
2023-03-02 16:14:34 -0800

    fixing files

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,10 +12,10 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; str != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 	if (str[i] >= 'a' && str[i] <= 'z')
-		str[i] -= 32;
+		str[i] = (int)str[i] -  32;
 	}
 	return (str);
 }
